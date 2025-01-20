3ed8af34731fbe933de87c0d967d710382abdc9c
Matheus Roma <matheusroma.ctt@gmail.com>
2025-01-20 18:17:03 -0300

    Game seed

--- src/main/java/com/matheusromagit/dslist/entities/Game.java
+++ src/main/java/com/matheusromagit/dslist/entities/Game.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -21,25 +21,30 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Game() {		
-	}
-
-	public Game(Long id, String title, Integer yer, String genre, String plataform, String imgUrl,
+    public Game() {
+    }
+    
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-		
 		this.id = id;
 		this.title = title;
-		this.year = yer;
+		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+		this.longDescription = longDescription;		
 	}
 
 	public Long getId() {
@@ -58,12 +63,12 @@ public class Game {
 		this.title = title;
 	}
 
-	public Integer getYer() {
+	public Integer getYear() {
 		return year;
 	}
 
-	public void setYer(Integer yer) {
-		this.year = yer;
+	public void setYear(Integer year) {
+		this.year = year;
 	}
 
 	public String getGenre() {
@@ -74,12 +79,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -122,9 +135,4 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-
 }
