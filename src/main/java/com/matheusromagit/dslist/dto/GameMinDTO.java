9fa94eb8e1c751692c5bef0fb8a464e702d75d1d
Matheus Roma <matheusroma.ctt@gmail.com>
2025-01-22 10:12:07 -0300

    Deploy ready

--- src/main/java/com/matheusromagit/dslist/dto/GameMinDTO.java
+++ src/main/java/com/matheusromagit/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.matheusromagit.dslist.dto;
 
 import com.matheusromagit.dslist.entities.Game;
+import com.matheusromagit.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -8,58 +9,41 @@ public class GameMinDTO {
 	private String title;
 	private Integer year;
 	private String imgUrl;
-	private String longDescription;
-
-	public GameMinDTO() {
-
-	}
-
+	private String shortDescription;
+	
 	public GameMinDTO(Game entity) {
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
-		longDescription = entity.getLongDescription();
+		shortDescription = entity.getShortDescription();
+	}
+
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getTitle() {
 		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 
 	public Integer getYear() {
 		return year;
 	}
 
-	public void setYear(Integer year) {
-		this.year = year;
-	}
-
 	public String getImgUrl() {
 		return imgUrl;
 	}
 
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
+	public String getShortDescription() {
+		return shortDescription;
 	}
-
-	public String getLongDescription() {
-		return longDescription;
-	}
-
-	public void setLongDescription(String longDescription) {
-		this.longDescription = longDescription;
-	}
-
 }
--- src/main/java/com/matheusromagit/dslist/services/GameService.java
+++ src/main/java/com/matheusromagit/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.matheusromagit.dslist.dto.GameDTO;
 import com.matheusromagit.dslist.dto.GameMinDTO;
 import com.matheusromagit.dslist.entities.Game;
+import com.matheusromagit.dslist.projections.GameMinProjection;
 import com.matheusromagit.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
